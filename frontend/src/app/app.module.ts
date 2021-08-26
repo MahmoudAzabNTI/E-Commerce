5516d6a1064396a63073d2d73243bebb7e25aec2
MahmoudAzabNTI <mahmoud.azab.nti@gmail.com>
2021-08-26 22:48:28 +0200

    strucure frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllUsersComponent } from './admin/pages/users/all-users/all-users.component';
+import { SingleUserComponent } from './admin/pages/users/single-user/single-user.component';
+import { AddUserComponent } from './admin/pages/users/add-user/add-user.component';
+import { RegisterUserComponent } from './site/pages/auth/register-user/register-user.component';
+import { LoginUserComponent } from './site/pages/auth/login-user/login-user.component';
+import { LoginAdminComponent } from './adminpages/auth/login-admin/login-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllUsersComponent,
+    SingleUserComponent,
+    AddUserComponent,
+    RegisterUserComponent,
+    LoginUserComponent,
+    LoginAdminComponent
   ],
   imports: [
     BrowserModule,
